ae5904a6f91d55d39b7f07ec81894f5018e8ecd2
Rohan Nag <nagrohan579@gmail.com>
2023-08-15 13:38:14 +0530

    Added The Title Screen and Input of Player Names

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,38 @@
+import java.util.Scanner;
+
 public class TicTacToe
 {
+    static char[][] board;
+    static int filledBoxesCount; 
+
+    TicTacToe()
+    {
+        board = new char[3][3];
+        filledBoxesCount = 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("WELCOME TO THE TIC TAC TOE GAME!");
+        System.out.println("\nWELCOME TO THE TIC TAC TOE GAME!");
+        System.out.println("This a Simple Terminal Based Tci Tac Toe game for 2 players.");
+        System.out.println("============================================================\n");
+
+        Player p1 = new Player(); 
+        Player p2 = new Player(); 
+
+        try(Scanner sc = new Scanner(System.in))
+        {
+            System.out.print("Enter name of first player: ");
+            p1.name = sc.nextLine();
+            p1.symbol = 'X';
+            System.out.print("Enter name of second player: ");
+            p2.name = sc.nextLine();
+            p2.symbol = 'O';
+        }
+        
         System.out.println();
+        System.out.println(String.format("%s will play as %c", p1.name,p1.symbol));
+        System.out.println(String.format("%s will play as %c", p2.name, p2.symbol));
+        
     }
 }
 
